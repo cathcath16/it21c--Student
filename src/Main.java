2ef750a2eeec68ed65fc12f56abeaed74617ba97
cathcath16 <catherenelabinisia@gmail.com>
2023-09-29 01:27:14 +0800

    added initial files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    
+    public static void main(String[] args) {
+    
+        Student student = new Student("Catherene T. Labinisia", "IT21C", "Set some goals, then demolish them");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+    }
+}
